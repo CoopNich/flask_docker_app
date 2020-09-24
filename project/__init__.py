2926af660ad4d8b6ef69e9eb8c994cd48775fd91
CoopNich <CoopNich92@gmail.com>
2020-09-24 14:17:33 -0500

    initial commit/requirements installed

---
+++ project/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+
+
+# instantiate the app
+app = Flask(__name__)
+
+api = Api(app)
+
+app.config.from_object('project.config.DevelopmentConfig')
+
+
+class Ping(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+
+api.add_resource(Ping, '/ping')
