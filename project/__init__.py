d97c09e7f10ad0425f45bbc5dfbd1c521c7ad49b
Kathryn Wohl <ktwohl@gmail.com>
2020-09-25 12:51:19 -0500

    docker and postgress-db configured

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -7,7 +9,24 @@ app = Flask(__name__)
 
 api = Api(app)
 
-app.config.from_object('project.config.DevelopmentConfig')
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings) 
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class BudgetItem(db.Model):
+    __tablename__ = 'budget_items'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), nullable=False)
+    cost = db.Column(db.String(128), nullable=False)
+    paid = db.Column(db.Boolean(), default=False, nullable=False)
+
+    def __init__(self, name, cost):
+        self.name = name
+        self.cost = cost
 
 
 class Ping(Resource):
