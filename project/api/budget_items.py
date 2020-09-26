1ba9521b0d2f1fed20e0bc69b78b5e28af387805
coopnich <coopnich92@gmail.com>
2020-09-25 20:11:54 -0500

    endpoints and tests for GET and POST complete

--- project/api/budget_items.py
+++ project/api/budget_items.py
@@ -20,13 +20,20 @@ budget_item = api.model('BudgetItem', {
 class BudgetItems(Resource):
     @api.marshal_with(budget_item)
     def get(self, budget_item_id):
-        return BudgetItem.query.filter_by(id=budget_item_id).first(), 200    
+        user = BudgetItem.query.filter_by(id=budget_item_id).first()
+        if not user:
+            api.abort(404, f"Item {budget_item_id} does not exist")
+        return user, 200   
 
 
 class BudgetItemsList(Resource):
     @api.marshal_with(budget_item)
     def get(self, budget_item_id):
         return BudgetItem.query.filter_by(id=budget_item_id).first(), 200
+
+    @api.marshal_with(budget_item, as_list=True)
+    def get(self):
+        return BudgetItem.query.all(), 200
 
     @api.expect(budget_item, validate=True)
     def post(self):
--- project/tests/test_budget_items.py
+++ project/tests/test_budget_items.py
@@ -1,6 +1,5 @@
 import json
 
-from project import db
 from project.api.models import BudgetItem
 
 # test for succesful post
@@ -75,15 +74,40 @@ def test_add_budget_item_duplicate(test_app, test_database):
 
 # test to GET single budget item
 
-def test_single_budget_item(test_app, test_database):
-    budget_item = BudgetItem(name='car', cost="250.00")
-    db.session.add(budget_item)
-    db.session.commit()
+def test_single_budget_item(test_app, test_database, add_budget_item):
+    budget_item = add_budget_item(name='car', cost="250.00")
     client = test_app.test_client()
     resp = client.get(f'/budget_items/{budget_item.id}')
     data = json.loads(resp.data.decode())
     assert resp.status_code == 200
     assert 'car' in data['name']
     assert '250.00' in data['cost']
+
+# test to GET all budget items
+
+def test_all_items(test_app, test_database, add_budget_item):
+    test_database.session.query(BudgetItem).delete()
+    add_budget_item('insurance', '200.00')
+    add_budget_item('electric', '100.00')
+    client = test_app.test_client()
+    resp = client.get('/budget_items')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert len(data) == 2
+    assert 'insurance' in data[0]['name']
+    assert '200.00' in data[0]['cost']
+    assert 'electric' in data[1]['name']
+    assert '100.00' in data[1]['cost']
+
+# test if id does not exist 
+
+def test_single_budget_item_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get('/budget_items/999')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'Item 999 does not exist' in data['message']
+
+
 
 
